674f20cbc788f3806cdc03f37bd90385358e10e8
alx.skryabin <alx.skryabin@gmail.com>
2021-11-21 21:04:49 +0300

    add lib materialize

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <div className="form">Форма</div>
       <div className="result">Калории</div>
       <div className="imt">ИМТ</div>
+      <div className="weight">Вес</div>
     </div>
   )
 }
