fa8b47cbaf01ac56765094b4573136ad6881d1de
A.Skriabin <A.Skriabin@asntl.ru>
2021-12-02 14:54:06 +0300

    finish home page

--- src/components/Nav/Sidenav.js
+++ src/components/Nav/Sidenav.js
@@ -3,15 +3,22 @@ import logo from '../../assets/img/logo.png'
 import {Link} from 'react-router-dom'
 
 export default function Sidenav() {
+  let sidenav = null
+
   useEffect(() => {
-    window.M.Sidenav.init(document.querySelectorAll('.sidenav'))
-  }, [])
+    let $sidenav = document.querySelectorAll('.sidenav')
+    sidenav = window.M.Sidenav.init($sidenav)[0]
+  }, [sidenav])
+
+  function closeNav() {
+    sidenav.close()
+  }
 
   return (
     <ul id="slide-out" className="sidenav">
       <div>
         <li>
-          <div className="user-view">
+          <div className="user-view" onClick={closeNav}>
             <div className="background">
               <img
                 src="https://static.tildacdn.com/tild3663-3763-4535-b139-363863386261/-/resize/504x/1587048064_11-p-khim.png"
@@ -26,10 +33,10 @@ export default function Sidenav() {
           </div>
         </li>
         <li><span className="subheader" style={{marginLeft: '30px'}}>Калькуляторы</span></li>
-        <li className="nav-icon">
+        <li className="nav-icon" onClick={closeNav}>
           <Link to="/calorie"><i className="fas fa-candy-cane"/> Калькулятор калорий</Link>
         </li>
-        <li className="nav-icon">
+        <li className="nav-icon" onClick={closeNav}>
           <Link to="/imt"><i className="fas fa-fire-alt"/> Индекс массы тела</Link>
         </li>
       </div>
--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import Title from '../../components/Content/title'
+import {Link} from 'react-router-dom'
 
 export default class Home extends React.Component {
-  state = {}
-
   render() {
     return (
       <div className="h_app">
         <Title num="1"/>
+        <div className="h_wrap-apps">
+          <div className="h_apps-item">
+            <Link to="/calorie" className="hoverable">
+              <i className="fas fa-candy-cane"/> Калькулятор калорий
+            </Link>
+          </div>
+          <div className="h_apps-item">
+            <Link to="/imt" className="hoverable">
+              <i className="fas fa-fire-alt"/> Индекс массы тела
+            </Link>
+          </div>
+        </div>
       </div>
     )
   }
