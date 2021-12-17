b30b8e49e4b9da4f5dfa03ad10d1fc3bfe199e01
alx.skryabin <alx.skryabin@gmail.com>
2021-12-18 00:51:33 +0300

    profi ru task

--- src/page/demo/Demo.js
+++ src/page/demo/Demo.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
 export default class Demo extends React.Component {
+  execPositiveNumberOperation(...args) {
+    return args
+      .filter(item => +item && +item > 0)
+      .map(item => +item)
+      .reduce((acc, item, i) => {
+        return (i === 0) ? item : eval(`${acc} ${args[0]} ${item}`)
+      }, null)
+  }
+
   render() {
     return (
       <div className="h_app">
         <div className="h_wrap-apps">
-          Demo React!
+          {this.execPositiveNumberOperation('+', '1', '2', 'a', 'b', '-1', '6')}<br/>
+          {this.execPositiveNumberOperation('-', '1', '2', 'a', 'b', '-1', '6')}<br/>
+          {this.execPositiveNumberOperation('*', '1', '2', 'a', 'b', '-1', '6')}<br/>
+          {this.execPositiveNumberOperation('/', '1', '2', 'a', 'b', '-1', '6')}<br/>
         </div>
       </div>
     )
